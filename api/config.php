4860681f17335f771f02ec7e059243422401540e
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2022-07-02 20:55:43 +0700

    fix

--- api/config.php
+++ api/config.php
@@ -11,4 +11,5 @@ $conn = new mysqli($servername, $username, $password, $database, 3306);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+//test
 $conn->set_charset("utf8");
